7696d3a2ca39c68b934bb9e31a3625528597ed9c
Lebogang <lebomz@outlook.com>
2026-01-18 18:25:14 +0200

    feat: integrate AWS Secrets Manager to retrieve Neo4j connection details

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,15 +1,30 @@
 import neo4j, { Driver } from 'neo4j-driver';
+import AWS from 'aws-sdk';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const client = new AWS.SecretsManager({
+  region: "eu-north-1",
+}); 
+const secret_name = "Neo4j";
+
+async function getNeo4jSecrets() {
+  const secret = await client.getSecretValue({ SecretId: secret_name }).promise();
+  return JSON.parse(secret.SecretString!);
+}
+
+/*
 const NEO4J_URI = process.env.NEO4J_URI;
 const NEO4J_USER = process.env.NEO4J_USER;
 const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD;
+*/
 
 let driver: Driver | null = null;
 
 export const connectDB = async (): Promise<void> => {
+    const { NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD } = await getNeo4jSecrets();
+
   if (!NEO4J_URI || !NEO4J_USER || !NEO4J_PASSWORD) {
     console.error('❌ Neo4j connection details are missing in environment variables.');
     process.exit(1);
